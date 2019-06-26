9bf7ff7eccc9c067f579aa1f406ff436112bffc2
Mahesh Rasakonda <mahesh.rasakonda@int.whishworks.com>
2019-06-26 16:06:45 +0530

    Remove gitcode

--- gitcode.bat
+++ gitcode.bat
@@ -4,7 +4,7 @@ git checkout master
 ::add all added/modified files
 git add .
 
-git commit -am "Add new text"
+git commit -am "Adding  new text"
 ::push to git remote repository
 
 git push --set-upstream "https://github.com/MaheshMahi496/CTRepository.git" master
